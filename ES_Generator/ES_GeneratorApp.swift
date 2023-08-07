9ff020adf9e5d1bb74c23a241da48be4100ea7dc
Kim <kokieng1195@gmail.com>
2023-08-08 00:51:02 +0900

    Initial Commit

---
+++ ES_Generator/ES_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ES_GeneratorApp.swift
+//  ES_Generator
+//
+//  Created by 木村洸貴 on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct ES_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
